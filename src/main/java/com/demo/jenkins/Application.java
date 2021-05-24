06ba9ed005e59edc8be70d39946a4af877bcd56e
Asha Subramanya <seetharamu.asha@gmail.com>
2021-05-24 11:49:25 -0400

    first commit

---
+++ src/main/java/com/demo/jenkins/Application.java
@@ -0,0 +1,26 @@
+package com.demo.jenkins;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+	
+	
+	public static final Logger log = LoggerFactory.getLogger(Application.class);
+
+	@PostConstruct
+	public void init() {
+		log.info("Application Started");
+	
+	}
+	public static void main(String[] args) {
+		log.info("Application executed ");
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/jenkins/ApplicationTests.java
@@ -0,0 +1,25 @@
+package com.demo.jenkins;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class ApplicationTests {
+	
+	public static Logger log = LoggerFactory.getLogger(ApplicationTests.class);
+
+	@Test
+	void contextLoads() {
+		log.info("Test Case executing....");
+		assertEquals(true, true);
+	}
+
+}
