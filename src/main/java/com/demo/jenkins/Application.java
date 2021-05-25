2e6f4dea46753a7a86a911f5ead711be370b6184
Asha Subramanya <seetharamu.asha@gmail.com>
2021-05-25 01:57:30 -0400

    added dependencies

--- src/main/java/com/demo/jenkins/Application.java
+++ src/main/java/com/demo/jenkins/Application.java
@@ -15,7 +15,7 @@ public class Application {
 
 	@PostConstruct
 	public void init() {
-		log.info("Application Started");
+		log.info("Application Started...");
 	
 	}
 	public static void main(String[] args) {
